c702e08906b1ce49191e3974c04880337a344571
Matthew Bartosh <matthewbartosh@gmail.com>
2022-07-07 20:57:20 -0500

    Finished player movement and screen boundaries.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,25 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Public or private reference
+// Data type (int, float, bool, string)
+// Every variable has a name
+// Optional value assigned
+
 public class Player : MonoBehaviour
 {
-    // Public or private reference
-    // Data type (int, float, bool, string)
-    // Every variable has a name
-    // Optional value assigned
-    public float speed = 3.5f;
+    [SerializeField]
+    private float _speed = 3.5f;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        // Take the current positoion = new position (0, 0, 0)
         transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(1, 0, 0) * 5 * Time.deltaTime);
+        CalculateMovement();
+     }
+
+    // CalculateMovement handles Player movement
+    void CalculateMovement()
+    {
+        float HorizontalInput = Input.GetAxis("Horizontal");
+        float VerticalInput = Input.GetAxis("Vertical");
+        
+        Vector3 direction = new Vector3(HorizontalInput, VerticalInput, 0);
+        
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+
+        if (transform.position.x > 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
     }
 }
