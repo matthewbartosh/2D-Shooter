43b20bccbf0de491697ef07ca19e425dfef2a819
Matthew Bartosh <matthewbartosh@gmail.com>
2022-07-13 13:48:31 -0500

    Spawn Manager added.

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    [SerializeField]
+    private GameObject _enemyContainer;
+    [SerializeField]
+    private float _waitTime = 5f;
+    private bool _stopSpawning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (_stopSpawning == false)
+        {
+            GameObject newEnemy = Instantiate(_enemyPrefab, new Vector3(Random.Range(-9.4f, 9.4f), 7.4f, 0), Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
+            yield return new WaitForSeconds(_waitTime);
+        }
+    }
+
+    public void OnPlayerDeath()
+    {
+        _stopSpawning = true;
+    }
+
+}
